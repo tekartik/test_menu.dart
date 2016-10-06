906b31b3a7059d0473e41da743bc7bf4e05fc7c6
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-07 00:13:47 +0200

    test: try chrome travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,9 +3,12 @@
 # Fast fail the script on failures.
 set -e
 
+# for debugging
+pub run chrome_travis:show_env
+
 dartanalyzer --fatal-warnings \
   lib/test_menu_browser.dart \
   lib/test_menu_mdl_browser.dart \
   lib/test_menu_console.dart
 
-pub run test -p vm,firefox
+pub run test -p vm,firefox,chrome
