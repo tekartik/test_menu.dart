84c1ae88ee31b38c08f532528a3a083d71c19ad4
Alexandre Roux <alex@tekartik.com>
2023-03-07 15:15:03 +0100

    fix process_run activation in io

--- test_menu_io/lib/src/vars.dart
+++ test_menu_io/lib/src/vars.dart
@@ -1,13 +1,20 @@
+import 'package:dev_test/build_support.dart';
 import 'package:process_run/shell.dart';
+
+Future<void> _processRunReady = () async {
+  await checkAndActivatePackage('process_run');
+}();
 
 /// Set an env var
 Future<void> setEnvVar(String key, String value) async {
+  await _processRunReady;
   await Shell().run(
       'dart pub global run process_run:shell env var set ${shellArgument(key)} ${shellArgument(value)}');
 }
 
 /// Delete an env var
 Future<void> deleteEnvVar(String key) async {
+  await _processRunReady;
   await Shell().run(
       'dart pub global run process_run:shell env var delete ${shellArgument(key)}');
 }
