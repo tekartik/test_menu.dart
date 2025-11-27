f9e243b447ca2ca0cece45afaf36d85f2c39ae23
Alexandre Roux <alex@tekartik.com>
2025-11-27 20:50:30 +0100

    fix try activating

--- test_menu_io/test/vars_test.dart
+++ test_menu_io/test/vars_test.dart
@@ -1,7 +1,11 @@
+import 'package:dev_build/build_support.dart';
 import 'package:tekartik_test_menu_io/key_value_io.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
+  setUpAll(() async {
+    await checkAndActivatePackage('process_run', verbose: true);
+  });
   test('vars test', () async {
     var key = 'eAEVYA0YTid8HNjBbdMb';
     await deleteEnvVar(key);
